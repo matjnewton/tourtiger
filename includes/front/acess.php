422ea6063e2039d87a1a1cbacbf865ae67162f90
Andriy <a.rysan@yahoo.com>
2016-11-08 22:57:41 +0200

    remove copy from git

--- includes/front/acess.php
+++
@@ -1,4 +0,0 @@
-<?php
-$allowed = 'http://escala.transfergest.com/front-end/';
-$allowed_forms = 'http://escala.transfergest.com/';
-?>
--- partials/content-tour_links.php
+++
@@ -1,9 +0,0 @@
-                        <?php $button_label = get_sub_field('button_label'); ?>
-                        <li class="col-sm-6 col-md-4">
-                            <div class="link-tour-wrapper">
-                                <div class="link-tour-name">
-                                    <span><?php the_title(); ?></span>
-                                </div>
-                                <a href="<?php the_permalink() ?>"><?php if($button_label): ?><span><?php echo $button_label; ?></span><?php endif; ?></a>
-                            </div>
-                        </li>
