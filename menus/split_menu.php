6ea0e92e806a9cd43afbfe4dce1b22e4bba02b7c
Andriy <a.rysan@yahoo.com>
2016-12-02 19:38:55 +0200

    add Languages menu aside Split menu

--- menus/split_menu.php
+++ menus/split_menu.php
@@ -3,6 +3,10 @@
     <div class="row<?php if($all_caps == true): ?> all-caps<?php endif; ?>">
                         
 <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'menu genesis-nav-menu main-nav', 'fallback_cb'    => false, 'container_class' => 'col-sm-5 col-md-5 col-lg-5 left-menu-part main-nav-wrapper', 'walker'  => new split_nav_walker() ) ); ?>
-
+<?php if($secondary_menu != true): ?>
+<?php if(function_exists('icl_object_id')): ?>
+                        <?php wp_nav_menu( array( 'theme_location' => 'language_switcher', 'menu_class' => 'lang-nav', 'container_class' => 'menu-languages-container', 'fallback_cb'    => false ) ); ?>
+                        <?php endif; ?>
+<?php endif; ?>
     </div>    
 </div>
