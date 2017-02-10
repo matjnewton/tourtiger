97ae88ddcd49548010caa095de75bff25f017f39
Saveliy <saveliy@etrange.com.ua>
2017-02-10 10:59:47 +0200

    Closed divider issue

--- includes/primary-content/section/pc-section-define-bottom-divider.php
+++ includes/primary-content/section/pc-section-define-bottom-divider.php
@@ -2,9 +2,9 @@
 
 	$tour_section_bd_repeater = '';
 	$tour_section_bd_image = '';
-	$tour_section_bd_class = 'pc_divider pc_divider__bottom js-divider';
+	$tour_section_bd_class = 'pc_divider pc_divider__bottom';
 
-	if ( get_sub_field( 'tour_pc-td--select' ) == 'repeater' ) {
+	if ( get_sub_field( 'tour_pc-bd--select' ) == 'repeater' ) {
 		$tour_section_bd_repeater = get_sub_field( 'tour_pc-bd--select__repeater' );
 
 		$tour_section_classes .= ' pc--s__bd_repeater pc--s__divider_repeater';
@@ -12,7 +12,7 @@
 		if ( !$tour_section_bd_repeater ) {
 			$tour_section_classes .= ' pc--s__bd_repeater_empty';
 		}
-	} elseif ( get_sub_field( 'tour_pc-td--select' ) == 'image' ) {
+	} elseif ( get_sub_field( 'tour_pc-bd--select' ) == 'image' ) {
 		$tour_section_bd_image = get_sub_field( 'tour_pc-bd--select__image' );
 
 		$tour_section_classes .= ' pc--s__bd_image';
