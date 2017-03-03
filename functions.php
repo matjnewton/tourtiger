f3b70117a18adeac92cc666c3498a0518702dbf2
Andriy <a.rysan@yahoo.com>
2017-03-03 21:57:45 +0200

    fix workaround for conflict between Wp Sass and Wp Rocket plugins

--- functions.php
+++ functions.php
@@ -1496,5 +1496,13 @@ function rem_wp_ver_css_js( $src ) {
 add_filter( 'style_loader_src', 'rem_wp_ver_css_js', 9999 );
 add_filter( 'script_loader_src', 'rem_wp_ver_css_js', 9999 );
 
+/*we need this if Wp Rocket plugin is active*/
+if (function_exists('rocket_clean_domain')) {
+add_action( 'wp_footer', 'add_theme_scss', 9999 );
+function add_theme_scss() { ?>
+<link rel="stylesheet" id="theme-css-footer" href="<?php bloginfo( 'url' ); ?>/wp-content/uploads/wp-sass-cache/theme.css" type="text/css" media="all">
+<?php }
+}
+
 /* update dgamoni */
 require_once 'inc/load.php'; 
