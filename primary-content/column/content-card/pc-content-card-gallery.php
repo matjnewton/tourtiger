5c8885b8f395ed30f41d732393cf91ac0059e247
Saveliy <saveliy@etrange.com.ua>
2017-03-07 18:20:48 +0200

    Revert "Upgraded scripts"
    This reverts commit fb94341a48436603342a3603b14002b7b2c53c4c.

--- primary-content/column/content-card/pc-content-card-gallery.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-if ( get_sub_field( 'tour_pc-coltype--gallery_type' ) == 'gallery' ) {
-	include( get_stylesheet_directory() . '/includes/primary-content/column/content-card/pc-content-card-gallery-pic.php' );
-} else {
-	include( get_stylesheet_directory() . '/includes/primary-content/column/content-card/pc-content-card-gallery-ins.php' );
-	
-} ?>
