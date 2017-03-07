1ee9693b1391edb3ce90df4d8a8d4d8df27b589e
Saveliy <saveliy@etrange.com.ua>
2017-03-07 18:21:07 +0200

    Revert "Revert "Revert "Upgraded scripts"""
    This reverts commit 80d7681218abff48f8ebe08571812ddb70385c9f.

--- primary-content/column/content-card/pc-content-card-init-headline.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$cc_style__ccc_css = pc_init_font_css( get_sub_field( 'cc_style__headline' ) );
-$cc_style__ccc_css[1] .= get_sub_field( 'cc_style__headline-color' ) ? 'color:' . get_sub_field( 'cc_style__headline-color' ) . ';' : '';
-
-echo $cc_style__ccc_css[0] ? $cc_style__ccc_css[0] : '';
-echo $cc_style__ccc_css[1] ? '#pc_wrap .' . $cc_style . ' div.pc--c__headline > * {' . $cc_style__ccc_css[1] . '}' : '';
--- primary-content/dependences/pc-dependences.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-include( get_stylesheet_directory() . '/includes/primary-content/column/content-card/pc-content-card-func.php' );
-include( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-flexiprod-card-func.php' );
-include( get_stylesheet_directory() . '/includes/primary-content/dependences/pc-crop-img.php' );
