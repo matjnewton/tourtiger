ed61c5013c656b56c1f07ac7961f9f2f68c5a066
Saveliy <saveliy@etrange.com.ua>
2017-04-04 19:35:40 +0300

    FIX

--- includes/primary-content/column/pc-column-loop.php
+++ includes/primary-content/column/pc-column-loop.php
@@ -3,8 +3,6 @@
 	$tour_column_content_classes = 'pc--c ';
 	$tour_column_content_styles = '';
 
-	if ( $include_margins ) echo '<div class="pc--c__margin_div">';
-
 	if ( get_row_layout() == 'tour_pc-content' ) {
 
 		include( get_stylesheet_directory() . '/includes/primary-content/column/content-card/pc-content-card.php' );
@@ -14,7 +12,5 @@
 		include( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-flexiprod-card.php' );
 	
 	}
-
-	if ( $include_margins ) echo '</div>';
 
 endwhile; ?>
