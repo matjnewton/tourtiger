50665e27a8fbbb8a5b06ac8cb24d8fff204810ae
Andriy <a.rysan@yahoo.com>
2017-04-07 00:53:39 +0300

    fix testimonials quote

--- content-scrltstmls_gpm.php
+++ content-scrltstmls_gpm.php
@@ -1,4 +1,7 @@
-
+<?php 
+                    $testimonial_quote = get_post_meta( get_the_ID(), 'testimonial_quote', true );
+                    
+                ?>
                 <li class="t-slide slide">
                     <div class="txt-testimonial"><span class="name"><?php the_title(); ?>:</span> <?php echo $testimonial_quote; ?><div><a href="<?php echo esc_url( home_url() );?>/testimonials/" class="read-test">Read full Testimonial</a></div>
                     </div>
