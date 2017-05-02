5930e5eb953acb7dac86eee10f22b46930131fa5
Saveliy <saveliy@etrange.com.ua>
2017-05-02 13:12:02 +0300

    Fix gallery issue

--- inc/functions/product_functions.php
+++ inc/functions/product_functions.php
@@ -3,7 +3,7 @@
 // include script and css for product cpt
 add_action( 'wp_enqueue_scripts', 'product_scripts_method' ); 
 function product_scripts_method() {
-	wp_register_script( 'product_scripts', CORE_URL . '/js/product_scripts.js', array('jquery'), '', true );
+	wp_register_script( 'product_scripts', CORE_URL . '/js/product_scripts.js', array('jquery', 'bxslider_min_js'), '', true );
     wp_enqueue_script( 'product_scripts');
 	wp_localize_script( 'product_scripts', 'localize_var', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
 	wp_register_style('product_css', CORE_URL .'/css/product.css', array(),null, 'all');
