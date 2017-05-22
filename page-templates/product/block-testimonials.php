f00039832e2d7dc51e259062a72b34ba521a7aa4
Saveliy <saveliy@etrange.com.ua>
2017-05-22 14:51:11 +0300

    Remove clickable events from testimonial's title in product page

--- page-templates/product/block-testimonials.php
+++ page-templates/product/block-testimonials.php
@@ -11,7 +11,7 @@
 							<?php foreach ( $post_objects as $key => $post_object) { ?>
 							<?php $testimonial_link = get_field('testimonial_link', $post_object->ID); ?>
 								<li>
-									<a class="testimonial_title" href=""><?php echo get_the_title($post_object->ID); ?></a>
+									<a class="testimonial_title" href="javascript:" style="cursor: text;pointer-events: none;"><?php echo get_the_title($post_object->ID); ?></a>
 									<div class="testimonial_quote"> <?php echo get_field('testimonial_quote', $post_object->ID); ?></div>
 								    <?php if($testimonial_link) : ?>
 								    	<a class="testimonial_link" href="<?php echo $testimonial_link; ?>" target="_blank">Read more</a>
