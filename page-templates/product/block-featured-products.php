85d3f7d09b7a8ed555a16eae3e451e3320b99f4f
Saveliy <saveliy@etrange.com.ua>
2017-06-19 17:32:20 +0300

    feature products view

---
+++ page-templates/product/block-featured-products.php
@@ -0,0 +1,59 @@
+<?php 
+/**
+ * Product primary area component: Featured Products
+ */
+
+if ( get_row_layout() == 'primary_content_f_products' && current_user_can( 'edit_posts' ) ) :
+
+    global $primary_content_options_count;
+	$post_objects = get_sub_field('featured_products');
+
+	if ( $post_objects ) : 
+		?>
+
+		<div class="product_content_wrapper primary_content_featured-products">
+			<div class="pc_featured-products-carousel">
+
+				<?php 
+				/**
+				 * Roll each selected product
+				 */
+				foreach ( $post_objects as $key => $post_object ) : 
+
+					/**
+					 * Get thumbnail
+					 */
+					if ( has_post_thumbnail( $post_object->ID ) ) {
+					    $thumb_id = get_post_thumbnail_id( $post_object->ID );
+					    $thumb_url = wp_get_attachment_image_src($thumb_id, 'large', true);
+					    $this_thumb = $thumb_url[0];
+					} else { 
+						$this_thumb = '//placehold.it/250x250';
+					}
+
+					?>
+
+					<div class="pc_featured-products">
+						<div class="pc_featured-products__thumb">
+							<a href="<?php the_permalink( $post_object->ID ); ?>" class="pc_featured-products__thumb-link">
+								<img src="<?=$this_thumb;?>" alt="<?php echo get_the_title( $post_object->ID ); ?>" class="pc_featured-products__img" />
+							</a>
+						</div>
+
+						<div class="pc_featured-products__body">
+							<h6><?php echo get_the_title( $post_object->ID ); ?></h4>
+							<a href="<?php the_permalink( $post_object->ID ); ?>" class="pc_featured-products__body-link">View Tour Now</a>
+						</div>
+					</div>
+
+					<?php 
+				endforeach; 
+				?>
+
+			</div>
+		</div>
+
+		<?php 
+	endif;
+
+endif;  
