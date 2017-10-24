8d23d5185d12a85a04b581a2dc89454d5f07eeac
Saveliy <dzvonkevich@gmail.com>
2017-10-24 18:02:55 +0300

    add ability to use  margins to headlines in hero area

--- includes/primary-content/head/temp/pc-elements.php
+++ includes/primary-content/head/temp/pc-elements.php
@@ -9,15 +9,22 @@
 
         <?php 
             for ( $i = 1; $i < 4; $i++ ) {
-                $title = get_sub_field( 'pc_ha_' . $i . '-tit' );
-                $tag = get_sub_field( 'pc_ha_' . $i . '-tit_seo' );
-                $hr = get_sub_field( 'pc_ha_' . $i . '-tit_hr' );
-                $hr_c = get_sub_field( 'pc_ha_' . $i . '-tit_hr-c' ) ? get_sub_field( 'pc_ha_' . $i . '-tit_hr-c' ) : '#333';
-                $hr_w = get_sub_field( 'pc_ha_' . $i . '-tit_hr-w' );
-                $hr_i = get_sub_field( 'pc_ha_' . $i . '-tit_hr-i' ) ? get_sub_field( 'pc_ha_' . $i . '-tit_hr-i' ) : '';
+                $title    = get_sub_field( 'pc_ha_' . $i . '-tit' );
+                $title_mt = get_sub_field( 'pc_ha_' . $i . '-tit-mt' ) ? get_sub_field( 'pc_ha_' . $i . '-tit-mb' ) . 'px' : '';
+                $title_mb = get_sub_field( 'pc_ha_' . $i . '-tit-mb' ) ? get_sub_field( 'pc_ha_' . $i . '-tit-mb' ) . 'px' : '';
+                $tag      = get_sub_field( 'pc_ha_' . $i . '-tit_seo' );
+                $hr       = get_sub_field( 'pc_ha_' . $i . '-tit_hr' );
+                $hr_c     = get_sub_field( 'pc_ha_' . $i . '-tit_hr-c' ) ? get_sub_field( 'pc_ha_' . $i . '-tit_hr-c' ) : '#333';
+                $hr_w     = get_sub_field( 'pc_ha_' . $i . '-tit_hr-w' );
+                $hr_i     = get_sub_field( 'pc_ha_' . $i . '-tit_hr-i' ) ? get_sub_field( 'pc_ha_' . $i . '-tit_hr-i' ) : '';
 
                 if ( $title ) {
-                    echo $title ? '<' . $tag . ' class="hero-area_title-' . $i . '">' . $title . '</' . $tag . '>' : '';
+                    $style = '';
+                    $style .= $title_mt ? 'margin-top:' . $title_mt . ';' : '';
+                    $style .= $title_mb ? 'margin-bottom:' . $title_mb . ';' : '';
+                    $style = $title_mt || $title_mb ? "style='{$style}'" : '';
+
+                    echo $title ? '<' . $tag . ' class="hero-area_title-' . $i . '" ' . $style . '>' . $title . '</' . $tag . '>' : '';
                     echo $hr && !$hr_i ? '<hr class="pc_ha_hr pc_ha_hr-' . $hr_w . '" style="color:' . $hr_c . ';">' : '';
                     echo $hr && $hr_i ? '<img class="pc_ha_hr-img" src="'. $hr_i . '" alt="" />' : '';
                 }
