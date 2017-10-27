6bfa9cec59209995d84eb1187c38cf3ac7c191b2
Saveliy <dzvonkevich@gmail.com>
2017-10-27 12:43:31 +0300

    add gallery styling css rules to primary area

--- includes/primary-content/column/content-card/pc-content-card-func.php
+++ includes/primary-content/column/content-card/pc-content-card-func.php
@@ -36,6 +36,8 @@ function get_pc_content_card_style( $cc_style ) {
 
 				include ( PCA_DIR . '/column/content-card/pc-content-card-init-video.php' );
 
+				include ( PCA_DIR . '/column/content-card/pc-content-card-init-gallery.php' );
+
 				echo '</style>';
 			} 
 		} 
