151c9776a4064e4483df2179ab9c5681d2e8904d
Saveliy <dzvonkevich@gmail.com>
2017-12-12 17:18:25 +0200

    new styling group: Core. Base styling components implemented

--- includes/primary-content/styling/pc-class-styling-core.php
+++ includes/primary-content/styling/pc-class-styling-core.php
@@ -27,26 +27,36 @@ class Core extends StylingCard {
 			'collapsed' => '',
 			'min' => 1,
 			'max' => 1,
-			'layout' => 'row',
+			'layout' => 'block',
 			'button_label' => 'Add Row',
 			'sub_fields' => array(
         array (
-          'key' => 'fc_' . $i . '_iI_58311',
-          'label' => 'Base',
-          'name' => 'base-msg',
-          'type' => 'message',
+          'key'      => 'fc_' . $i . '_iI_58311',
+          'label'    => 'Base',
+          'name'     => 'base-msg',
+          'type'     => 'message',
           'required' => 0,
-          'message' => 'Base styles'
+          'message'  => 'Base styles'
         ),
         array (
           'key' => 'fc_' . $i . '_iI_58312',
-          'label' => 'Label Font Color',
-          'name' => 'fc_style__la_butt_font-color',
+          'label' => 'Wrapper Background',
+          'name' => 'base-wrapper-bg',
           'type' => 'rgba_color',
           'required' => 0,
-          'return_value' => 0,
-          'ext_value' => array (
-          ),
+          'wrapper' => array(
+            'width' => 50,
+          )
+        ),
+        array (
+          'key' => 'fc_' . $i . '_iI_67403',
+          'label' => 'Wrapper Box-shadow',
+          'name' => 'base-wrapper-bs',
+          'type' => 'true_false',
+          'required' => 0,
+          'wrapper' => array(
+            'width' => 50,
+          )
         ),
       ),
 		);	
