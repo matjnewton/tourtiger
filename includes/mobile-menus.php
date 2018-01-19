644667316f25f77a72117f7e13e65b1b8533dddd
Saveliy <dzvonkevich@gmail.com>
2018-01-19 10:30:53 +0000

    small roll back around peek integration

--- includes/mobile-menus.php
+++ includes/mobile-menus.php
@@ -117,14 +117,14 @@ class Wpse8170mobile_Menu_Walker extends Walker_Nav_Menu {
             $h_attribute .= ' itemprop="url"';
             $h_attribute .= ' data-purchase-type="gift-card"';
             $h_attribute .= ' data-button-text="'.$item->title.'"';
-            $h_attributes .= ' data-partner-gid="'.$gid.'"';
+            $h_attribute .= ' data-partner-gid="'.$gid.'"';
           else:
             $h_attribute = ' href="https://www.peek.com/s/'.$gid.'"';
             $h_attribute .= ' class="peek-book-button-flat"';
             $h_attribute .= ' itemprop="url"';
             $h_attribute .= ' data-purchase-type="activity"';
             $h_attribute .= ' data-button-text="'.$item->title.'"';
-            $h_attributes .= ' data-activity-gid="'.$gid.'"';
+            $h_attribute .= ' data-activity-gid="'.$gid.'"';
           endif;
 
           $item_output = $args->before;
