87e2708cc8d1d24d1ccf8701ba67b13ee1532d09
Saveliy <dzvonkevich@gmail.com>
2018-02-05 14:51:29 +0000

    * changed mobile device detector in hero area

--- includes/primary-content/head/temp/pc-banner-video.php
+++ includes/primary-content/head/temp/pc-banner-video.php
@@ -6,15 +6,14 @@
         muted 
         poster="<?php if ( $poster_url ) echo $poster_url; ?>" 
         id="video1" 
-        class="pc_hero-area__video">
+        class="pc_hero-area__video flxslider-wrapper--video">
             <source src="<?php echo $hero_video; ?>" type="video/mp4">
             <source src="<?php echo $hero_video_webm; ?>" type="video/webm">
             <source src="<?php echo $hero_video_ogv; ?>" type="video/ogv">
     </video>
-<?php endif;
-$mobile_styles = wp_is_mobile() ? "background: url({$poster_url}) 50% 50% no-repeat; background-size: cover;" : ''; ?>
+<?php endif; ?>
 
-<div class="flxslider-wrapper" style="position: relative;z-index: 5;<?=$mobile_styles;?>">
+<div class="flxslider-wrapper flxslider-wrapper--bg" style="position: relative;z-index: 5;background: url(<?=$poster_url;?>) 50% 50% no-repeat; background-size: cover;">
     <div id="slider">
         <ul class="slides">
 
