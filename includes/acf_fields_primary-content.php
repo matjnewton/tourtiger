f2e535de7b1fd7d8fca46c8930a5d431183ebded
Saveliy <dzvonkevich@gmail.com>
2018-02-05 18:00:42 +0000

    implement theme-ver iteration to improve purging the cache

--- includes/acf_fields_primary-content.php
+++ includes/acf_fields_primary-content.php
@@ -615,16 +615,18 @@ add_action( 'wp_enqueue_scripts', 'primary_area_deregister_unnesesarily_scripts'
 
 function primary_area_deregister_unnesesarily_scripts() {
 
+  $theme = wp_get_theme();
+
 	if ( is_page_template( 'page-templates/test-pc.php' ) ) :
 
-		wp_enqueue_style('pc-constructor', get_stylesheet_directory_uri() . '/includes/primary-content/dependences/pc.min.css', null, 3 );
+		wp_enqueue_style('pc-constructor', get_stylesheet_directory_uri() . '/includes/primary-content/dependences/pc.min.css', false, $theme->get( 'Version' ) );
 		wp_enqueue_style('pc-bootstrap-select', '//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/css/bootstrap-select.min.css' );
 		wp_enqueue_script('hmac-sha1-js', 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/hmac-sha1.js', false, null, true );
 		wp_enqueue_script('enc-base64-min-js', 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/components/enc-base64-min.js', false, null, true );
 		wp_enqueue_script('pc-bootstrap-select', '//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.2/js/bootstrap-select.min.js', false, null, true );
 
 		wp_enqueue_script('mask', get_stylesheet_directory_uri() . '/includes/primary-content/assets/js/jquery.mask.js', false, null, true );
-		wp_enqueue_script('pc-main-scripts', get_stylesheet_directory_uri() . '/includes/primary-content/assets/js/primary-content.min.js', false, null, true );
+		wp_enqueue_script('pc-main-scripts', get_stylesheet_directory_uri() . '/includes/primary-content/assets/js/primary-content.min.js', false, $theme->get( 'Version' ), true );
 
 		//wp_dequeue_script('bootstrapjs');
 		wp_dequeue_script('colorbox');
