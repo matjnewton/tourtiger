685db95572fefdf9de887b13b2dfcf42b46981e6
Saveliy <dzvonkevich@gmail.com>
2018-07-30 18:15:16 +0300

    add some new gallery options

--- includes/primary-content/column/content-card/pc-content-card-gallery-pic.php
+++ includes/primary-content/column/content-card/pc-content-card-gallery-pic.php
@@ -1,9 +1,12 @@
-<?php 
+<?php
 /**
  * Content card gallery
  */
-$gallery = get_sub_field( 'tour_pc-coltype--gallery_add' );
-$label   = get_sub_field( 'tour_pc-coltype--gallery_label' );
+$gallery   = get_sub_field( 'tour_pc-coltype--gallery_add' );
+$label     = get_sub_field( 'tour_pc-coltype--gallery_label' );
+$columns   = get_sub_field( 'tour_pc-coltype--gallery_count' );
+$type      = get_sub_field( 'gallery_type' );
+$onclick   = get_sub_field( 'gallery_onclick' );
 
 include get_stylesheet_directory() . '/partials/gallery.php';
 ?>
