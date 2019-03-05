d7118614db5c5f924101d406e8e14609f767f2cb
sunneyman <open.air.xxi@gmail.com>
2019-03-05 17:22:33 +0200

    download speed optimization

--- includes/primary-content/section/pc-section-define-image.php
+++ includes/primary-content/section/pc-section-define-image.php
@@ -1,15 +1,15 @@
-<?php 
+<?php
 
 	$tour_section_bg_image_url      = get_sub_field( 'tour_pc-bg__select-image--image' );
 	$tour_section_bg_image_fixed    = get_sub_field( 'tour_pc-bg__select-image--fixed' );
     $tour_section_bg_image_expanded = get_sub_field( 'tour_pc-bg__select-image--expanded' );
 
-	$tour_section_bg_image = mr_image_resize( $tour_section_bg_image_url, 1920 );
+	$tour_section_bg_image =  $tour_section_bg_image_url;
 
 	$tour_section_classes .= ' pc--s__img';
 
     if ( $tour_section_bg_image != 'image_not_specified' ) {
-        if ( $tour_section_bg_image_fixed == 'yep' && $tour_section_bg_image ) { 
+        if ( $tour_section_bg_image_fixed == 'yep' && $tour_section_bg_image ) {
         	$tour_section_classes .= ' pc--s__img_fixed';
         	$tour_section_styles .= ' background: url(' . $tour_section_bg_image . ') center center no-repeat; background-attachment: fixed;background-size: cover;';
         } elseif ( $tour_section_bg_image_fixed != 'yep' && $tour_section_bg_image && $tour_section_bg_image_expanded != 'yep' ) {
