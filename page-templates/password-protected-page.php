43398e294de99154343117b16997ced493b833d3
sunneyman <open.air.xxi@gmail.com>
2019-03-27 22:25:45 +0200

    password-protected page improved

--- page-templates/password-protected-page.php
+++ page-templates/password-protected-page.php
@@ -6,18 +6,27 @@ $password_protected_page_field_after_password = get_field('password_protected_pa
 
 ?>
 
-
 <div class="password-form">
-    <h2><?php echo $password_protected_page_title;?></h2>
-    <p><?php echo $password_protected_page_subtitle;?></p>
+    <?php if($password_protected_page_title) : ?>
+        <h2><?php echo $password_protected_page_title;?></h2>
+    <?php endif; ?>
+    <?php if($password_protected_page_subtitle) : ?>
+        <p><?php echo $password_protected_page_subtitle;?></p>
+    <?php endif; ?>
     <form action="<?php echo esc_url( site_url( 'wp-login.php?action=postpass', 'login_post' ) ) ?>" class="post-password-form" method="post">
-        <p><?php echo __( $password_protected_page_text_above_password_field ) ?></p>
+        <?php if($password_protected_page_text_above_password_field) : ?>
+            <p><?php echo __( $password_protected_page_text_above_password_field ) ?></p>
+        <?php else : ?>
+            <p><?php echo __( 'This content is password protected. To view it please enter your password below.' ) ?></p>
+        <?php endif; ?>
         <p><label for="<?php echo $label; ?>"><?php echo __( 'Your password:' ); ?>
                 <input name="post_password" id="<?php echo $label; ?>" type="password" size="30" /></label>
             <input class="button js-pulsing" type="submit" name="Submit" value="<?php echo esc_attr_x( 'Enter', 'post password form' );?>" />
         </p>
     </form>
-    <p><?php echo $password_protected_page_field_after_password;?></p>
+    <?php if($password_protected_page_field_after_password) : ?>
+        <p><?php echo $password_protected_page_field_after_password;?></p>
+    <?php  endif; ?>
 </div>
 
-<?php apply_filters( 'the_password_form', $output ); ?>
+<?php apply_filters( 'the_password_form', $output ); ?> <!-- probably, this thing is not needed -->
