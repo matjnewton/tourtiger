0e70b078d9d0ee319cea9624e330e4f56242f16d
Saveliy <dzvonkevich@gmail.com>
2020-02-28 09:51:02 +0200

    Update class-410-redirect.php

--- inc/class-410-redirect.php
+++ inc/class-410-redirect.php
@@ -7,6 +7,23 @@ class Redirect410
     function __construct()
     {
         add_action('wp', __CLASS__.'::run_on_wp');
+        add_action('tt2/410/message', __CLASS__.'::render_410_message');
+    }
+
+
+    public static function render_410_message() {
+        $phone = get_option('options_phone_number');
+        $mail  = get_bloginfo('admin_email');
+        ?>
+
+        <div class="box wysiwyg primary-content padding_top padding_bottom" align="center">
+            <h3>Hey!<br/>We’re <?php echo get_bloginfo('name'); ?></h3>
+            <p><strong>Sorry to inform you, but the page you’re looking for has been permanently removed.</strong></p>
+            <p>Use the menu to navigate through our experiences or visit our <a href="/">homepage</a>.<br/>
+                You can also get in contact with us at <a href="tel:<?php echo preg_replace('/\D+/', '', $phone); ?>"><?=$phone;?></a> or by email at <?=$mail;?>.</p>
+        </div>
+
+        <?php
     }
 
 
@@ -14,15 +31,7 @@ class Redirect410
         global $wp_query;
 
         if (is_404() && isset($_SERVER['REQUEST_URI'])) :
-            $slug = self::get_slug_from_request($_SERVER['REQUEST_URI']);
-
-            $query = new \WP_Query([
-                'post_type' => 'any',
-                'post_status' => 'trash',
-                'pagename' => $slug.'__trashed',
-            ]);
-
-            if (isset($query->posts) && $query->posts && isset($query->posts[0])) :
+            if (self::is_trashed_post_of_page( self::get_slug_from_request($_SERVER['REQUEST_URI']) )) :
                 header( "HTTP/1.1 410 Gone" );
 
                 // is_410();
@@ -73,6 +82,30 @@ class Redirect410
         return '';
     }
 
+    private static function is_trashed_post_of_page() {
+        $query_post = new \WP_Query([
+            'post_type' => 'any',
+            'post_status' => 'trash',
+            // 'pagename' => $slug.'__trashed',
+            'post_name' => $slug.'__trashed',
+        ]);
+
+        if (isset($query_post->posts) && $query_post->posts && isset($query_post->posts[0])) :
+            return $query_post->posts[0]->ID;
+        else :
+            $query_page = new \WP_Query([
+                'post_type' => 'any',
+                'post_status' => 'trash',
+                'pagename' => $slug.'__trashed',
+            ]);
+
+            if (isset($query_page->posts) && $query_page->posts && isset($query_page->posts[0])) :
+                return $query_page->posts[0]->ID;
+            endif;
+        endif;
+
+        return false;
+    }
 }
 
 Redirect410::instance();
