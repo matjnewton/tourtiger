9cb44f30491ded8f7a1e845b93cec75240fe4bc0
sunneyman <open.air.xxi@gmail.com>
2020-04-09 09:48:42 +0300

    FIX: Space above the hero image on ttv1, https://sammyshawaii.com/

--- background_gpm.php
+++ background_gpm.php
@@ -17,7 +17,7 @@
     if($sticky_menu == true): ?>
 @media (min-width:481px){
     .banner-wrapper.under-header{
-        height:620px !important;
+<!--        height:620px !important;  -->
 
     }
 }
