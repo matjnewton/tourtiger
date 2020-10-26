aaf9977870a137f81393c87672c9df9ef45002c6
sunneyman <open.air.xxi@gmail.com>
2020-10-26 11:52:19 +0200

    Remove "Fix header on top of page" from PC constructor (conflicts with options setting)

--- includes/primary-content/dependences/pc-footer-inc.php
+++ includes/primary-content/dependences/pc-footer-inc.php
@@ -7,8 +7,8 @@
 <script type="text/javascript">
 
 	    $(function(){
-        	<?php if ( false && get_field('relative-header') ) ?>
-            $('.header-bar-wrapper').addClass('no-sticky');
+        	<?php // if ( get_field('relative-header') ) ?>
+            // $('.header-bar-wrapper').addClass('no-sticky');
 
             <?php if ( get_field('header-color') ) : ?>
                 $('.site-header').css('background-color', '<?php the_field('header-color') ?>' );
