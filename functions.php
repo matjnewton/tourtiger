883dbe2ce75ee294aea5b056433114c1772e91c6
sunneyman <open.air.xxi@gmail.com>
2022-11-14 16:43:39 +0200

    Move gf plugin to the theme

--- functions.php
+++ functions.php
@@ -30,7 +30,20 @@ function acf_settings_dir() {
 
 }
 
- include_once get_stylesheet_directory() . '/includes/plugins/acf-5.7.13/acf.php';
+include_once get_stylesheet_directory() . '/includes/plugins/acf-5.7.13/acf.php';
+
+
+// To use modified gf plugin version and avoid its updating
+function deactivate_plugin_conditional() {
+    if ( is_plugin_active('gravityforms/gravityforms.php') ) {
+        deactivate_plugins('gravityforms/gravityforms.php');
+    }
+}
+add_action( 'admin_init', 'deactivate_plugin_conditional' );
+
+// deactivate_plugins( '/gravityforms/gravityforms.php' );
+include_once get_stylesheet_directory() . '/includes/plugins/gravityforms/gravityforms.php';
+
 
 if ( ! class_exists( 'acf' ) )
     return;
