82ba6ceb269fb12ea0e3f3550713f8ccb2a352e0
sunneyman <open.air.xxi@gmail.com>
2022-11-17 13:53:38 +0200

    Fix issues with includes/primary-content/column/flexiprod-card/pc-flexiprod-card-func.php

--- includes/primary-content/column/flexiprod-card/pc-flexiprod-card-func.php
+++ includes/primary-content/column/flexiprod-card/pc-flexiprod-card-func.php
@@ -6,16 +6,16 @@ function get_pc_flexiprod_card_style( $fc_style ) {
 
 	if ( have_rows( $fc_style, 'option' ) ) {
 
-		while ( have_rows( $fc_style, 'option' ) ) { 
+		while ( have_rows( $fc_style, 'option' ) ) {
 
-			the_row(); 
+			the_row();
 
 			echo '<style>';
 
 			include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-init-flexi-default.php' );
 
-			/** 
-	         * Image 
+			/**
+	         * Image
 			 */
 			if (
 				( get_sub_field( 'fc_style__ov' ) != 'none' )
@@ -23,11 +23,12 @@ function get_pc_flexiprod_card_style( $fc_style ) {
 				|| get_sub_field( 'fc_style__imru' )
 			) include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-init-image-default.php' );
 
-			if ( 
-				in_array( 'title', $flexi_attr['image'] ) 
-				|| in_array( 'desc', $flexi_attr['image'] ) 
-				|| in_array( 'price', $flexi_attr['image'] ) 
-				|| in_array( 'label', $flexi_attr['image'] ) 
+			if (   isset($flexi_attr['image']) && is_array($flexi_attr['image']) && (
+					in_array( 'title', $flexi_attr['image'] )
+					|| in_array( 'desc', $flexi_attr['image'] )
+					|| in_array( 'price', $flexi_attr['image'] )
+					|| in_array( 'label', $flexi_attr['image'] )
+				)
 			) include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-init-image.php' );
 
 			/**
@@ -39,17 +40,18 @@ function get_pc_flexiprod_card_style( $fc_style ) {
 				$flexi_attr['prefix'] = $prefix;
 				$flexi_attr['name'] = $name;
 
-				if ( 
-					in_array( 'top-border', $flexi_attr[$prefix] ) 
-					|| in_array( 'title', $flexi_attr[$prefix] ) 
-					|| in_array( 'desc', $flexi_attr[$prefix] ) 
-					|| in_array( 'detail', $flexi_attr[$prefix] ) 
-					|| in_array( 'price', $flexi_attr[$prefix] ) 
-					|| in_array( 'button', $flexi_attr[$prefix] ) 
+				if ( isset($flexi_attr[$prefix]) && is_array($flexi_attr[$prefix]) && (
+						in_array( 'top-border', $flexi_attr[$prefix] )
+						|| in_array( 'title', $flexi_attr[$prefix] )
+						|| in_array( 'desc', $flexi_attr[$prefix] )
+						|| in_array( 'detail', $flexi_attr[$prefix] )
+						|| in_array( 'price', $flexi_attr[$prefix] )
+						|| in_array( 'button', $flexi_attr[$prefix] )
+					)
 				) include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-init-content.php' );
 
 			endforeach;
-		
+
 			echo '</style>';
 
 		}
