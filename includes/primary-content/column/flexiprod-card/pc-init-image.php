1975b25dbcf8af256a80e580e1e20b33a13b0623
sunneyman <open.air.xxi@gmail.com>
2022-11-17 14:25:16 +0200

    Fix: in_array() expects parameter 2 to be array, string given in themes/tourtiger/includes/primary-content/column/flexiprod-card/pc-init-image.php on line 29

--- includes/primary-content/column/flexiprod-card/pc-init-image.php
+++ includes/primary-content/column/flexiprod-card/pc-init-image.php
@@ -4,34 +4,42 @@
  */
 include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-init-image-text.php' );
 
-if ( in_array( 'title', $flexi_attr['image'] ) ) 
-	include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-init-image-title.php' );
+if ( isset($flexi_attr['image']) && is_array($flexi_attr['image']) ) :
 
-if ( in_array( 'desc', $flexi_attr['image'] ) ) 
-	include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-init-image-desc.php' );
+	if ( in_array( 'title', $flexi_attr['image'] ) )
+		include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-init-image-title.php' );
 
-if ( in_array( 'price', $flexi_attr['image'] ) ) 
-	include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-init-image-price.php' );
+	if ( in_array( 'desc', $flexi_attr['image'] ) )
+		include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-init-image-desc.php' );
 
-if ( in_array( 'label', $flexi_attr['image'] ) ) 
-	include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-init-image-label.php' );
+	if ( in_array( 'price', $flexi_attr['image'] ) )
+		include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-init-image-price.php' );
+
+	if ( in_array( 'label', $flexi_attr['image'] ) )
+		include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/pc-init-image-label.php' );
+
+endif;
 
 
 /**
  * Hover
  */
-if ( ! wp_is_mobile() ) : 
+if ( ! wp_is_mobile() ) :
 	include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/hover/pc-init-image-text.php' );
 
-	if ( in_array( 'title', $flexi_attr['hover'] ) ) 
-		include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/hover/pc-init-image-title.php' );
+	if ( isset($flexi_attr['hover']) && is_array($flexi_attr['hover']) ) :
 
-	if ( in_array( 'desc', $flexi_attr['hover'] ) ) 
-		include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/hover/pc-init-image-desc.php' );
+		if ( in_array( 'title', $flexi_attr['hover'] ) )
+			include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/hover/pc-init-image-title.php' );
 
-	if ( in_array( 'price', $flexi_attr['hover'] ) ) 
-		include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/hover/pc-init-image-price.php' );
+		if ( in_array( 'desc', $flexi_attr['hover'] ) )
+			include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/hover/pc-init-image-desc.php' );
 
-	if ( in_array( 'label', $flexi_attr['hover'] ) ) 
-		include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/hover/pc-init-image-label.php' );
+		if ( in_array( 'price', $flexi_attr['hover'] ) )
+			include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/hover/pc-init-image-price.php' );
+
+		if ( in_array( 'label', $flexi_attr['hover'] ) )
+			include ( get_stylesheet_directory() . '/includes/primary-content/column/flexiprod-card/hover/pc-init-image-label.php' );
+
+	endif;
 endif;
