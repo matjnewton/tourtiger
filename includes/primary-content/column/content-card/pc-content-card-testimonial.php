cb2241552cce1dc3d639b1125f4bd9730cf30e1b
sunneyman <open.air.xxi@gmail.com>
2023-06-29 15:18:40 +0200

    https://trello.com/c/IIFUXbdw/4740-hyperlink-reviews-ttv1

--- includes/primary-content/column/content-card/pc-content-card-testimonial.php
+++ includes/primary-content/column/content-card/pc-content-card-testimonial.php
@@ -1,17 +1,21 @@
 <?php
-	$tour_content_content_classes .= ' pc--c__testimonial';
+    if ( isset($tour_content_content_classes) )
+	    $tour_content_content_classes .= ' pc--c__testimonial';
+
 	$pc_content_testimonial = 'tour_pc-coltype--testimonial_ob';
 
-	while ( have_rows( $cc_style, 'option' ) ) {
+	while ( have_rows( $cc_style ?? [], 'option' ) ) {
 		the_row();
 
 		$cc_style__test_show = get_sub_field( 'cc_style__test_show' );
 	}
 
-	if( get_sub_field( $pc_content_testimonial ) ) : ?>
+    $cc_style__test_show = $cc_style__test_show ?? '';
+
+if( get_sub_field( $pc_content_testimonial ) ) : ?>
 		<div
-			class="<?php echo $tour_content_content_classes; ?>"
-			style="<?php echo $tour_content_content_styles; ?>">
+			class="<?php echo $tour_content_content_classes ?? ''; ?>"
+			style="<?php echo $tour_content_content_styles ?? ''; ?>">
 			<div class="pc--c__testimonial--slider js-new-slider">
 
 			 	<?php foreach( get_sub_field( $pc_content_testimonial ) as $post_object): ?>
@@ -19,7 +23,6 @@
 
 						<?php if (
 							get_field( 'photo', $post_object->ID )
-							&& in_array( 'logo', $cc_style__test_show )
 						) { ?>
 							<div class="pc--c__testimonial--logo">
 								<img
@@ -30,7 +33,6 @@
 
 						<?php if (
 							get_the_title($post_object->ID)
-							&& in_array( 'title', $cc_style__test_show )
 						) { ?>
 							<h3 class="pc--c__testimonial--title">
 								<?php echo get_the_title($post_object->ID); ?>
@@ -39,7 +41,6 @@
 
 						<?php if (
 							get_field( 'testimonial_quote', $post_object->ID )
-							&& in_array( 'excerpt', $cc_style__test_show )
 						) { ?>
 							<p class="pc--c__testimonial--description">
 								<?php echo get_field( 'testimonial_quote', $post_object->ID ); ?>
@@ -48,7 +49,6 @@
 
 						<?php if (
 							get_field( 'testimonial_link', $post_object->ID )
-							&& in_array( 'link', $cc_style__test_show )
 						) { ?>
 							<a
 								target="_blank"
