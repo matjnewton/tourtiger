b4e5f107949d55826db3c70d738ba6e0f135b82b
sunneyman <open.air.xxi@gmail.com>
2024-02-28 06:15:24 +0100

    Add gif support for featured-tours-2 images

--- content-feat_tours_22.php
+++ content-feat_tours_22.php
@@ -6,10 +6,11 @@ $button_label = get_sub_field('button_label');
     <?php if (has_post_thumbnail()) {
         $thumb = get_post_thumbnail_id();
         $img_url = wp_get_attachment_url( $thumb,'full'); //get img URL
+
         if($col==6):
-            $image = aq_resize( $img_url, 568, 377, true );
+            $image = str_contains($img_url, '.gif') ? $img_url : aq_resize( $img_url, 568, 377, true );
         else:
-            $image = aq_resize( $img_url, 377, 377, true ); //resize & crop img
+            $image = str_contains($img_url, '.gif') ? $img_url : aq_resize( $img_url, 377, 377, true ); //resize & crop img
         endif;
         ?>
         <a href="<?php the_permalink() ?>" class="tile-image">
